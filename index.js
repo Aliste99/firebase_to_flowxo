8524493188ddf3dc348b7f01b5a4cc5b85c5975c
Alina <alechka9906@gmail.com>
2017-12-28 16:25:44 +0000

    Add listener on child changes in FireBase database

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 var admin = require("firebase-admin");
 
-var serviceAccount = require("/task-d7f04c329dd3.json");
+var serviceAccount = require("./task-d7f04c329dd3.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://task-3d21a.firebaseio.com/"
+});
+
+// Get a database reference to our posts
+var db = admin.database();
+var ref = db.ref("messages");
+
+// Get the data on a post that has changed
+ref.on("child_changed", function(snapshot) {
+  var changedPost = snapshot.val();
+  console.log("The updated post title is " + changedPost.responsePath);
 });

48da9e30cb6dbf6c146317e0d48355ca44667fd4
Alina <alechka9906@gmail.com>
2018-01-04 09:55:51 +0000

    Add request to flowXO

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var admin = require("firebase-admin");
+const request = require('request');
 
 var serviceAccount = require("./task-d7f04c329dd3.json");
 
@@ -6,6 +7,7 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://task-3d21a.firebaseio.com/"
 });
+
 
 // Get a database reference to our posts
 var db = admin.database();
@@ -15,4 +17,11 @@ var ref = db.ref("messages");
 ref.on("child_changed", function(snapshot) {
   var changedPost = snapshot.val();
   console.log("The updated post title is " + changedPost.responsePath);
+  
+    request('https://flowxo.com/hooks/a/brvvb2vb?name=We%20did%20it', { json: true }, (err, res, body) => {
+      if (err) { return console.log(err); }
+      console.log(body.url);
+      console.log(body.explanation);
+    });
+  
 });

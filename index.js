18ca7dcd562d00c9d36c7bbe49f338d718e95f9b
Alina <alechka9906@gmail.com>
2018-01-05 10:20:08 +0000

    Change message reference to sheets reference. Ad on reff order by child and compare by new.

--- index.js
+++ index.js
@@ -8,20 +8,20 @@ admin.initializeApp({
   databaseURL: "https://task-3d21a.firebaseio.com/"
 });
 
-
 // Get a database reference to our posts
 var db = admin.database();
-var ref = db.ref("messages");
+var ref = db.ref("sheets");
 
 // Get the data on a post that has changed
-ref.on("child_changed", function(snapshot) {
+ref.orderByChild("status").equalTo("new").on("child_added", function(snapshot, prevChildKey) {
   var changedPost = snapshot.val();
   console.log("The updated post title is " + changedPost.responsePath);
-  
-    request('https://flowxo.com/hooks/a/brvvb2vb?name=We%20did%20it', { json: true }, (err, res, body) => {
-      if (err) { return console.log(err); }
-      console.log(body.url);
-      console.log(body.explanation);
-    });
-  
+    console.log(changedPost.sqlResult);
+//    request('https://flowxo.com/hooks/a/j3kbdake?path=' + changedPost.responsePath + changedPost, { json: true }, (err, res, body) => {
+//      if (err) { return console.log(err); }
+//      console.log(body.url);
+//      console.log(body.explanation);
+//    });
+
+    
 });

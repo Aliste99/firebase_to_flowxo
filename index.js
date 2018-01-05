9db4ec56e271f38b552690340365b8d19a6c4f6e
Alina <alechka9906@gmail.com>
2018-01-05 10:53:56 +0000

    Wrote an action plan and stub functions

--- index.js
+++ index.js
@@ -17,11 +17,28 @@ ref.orderByChild("status").equalTo("new").on("child_added", function(snapshot, p
   var changedPost = snapshot.val();
   console.log("The updated post title is " + changedPost.responsePath);
     console.log(changedPost.sqlResult);
+    
+    //TODO: Check presence of auth tokens
+    //TODO: If token present, create spreadsheet and send link,
+    //if not generate link, send to user, generate tokens,
+    // save tokens, create spreadsheet, send link
+    
 //    request('https://flowxo.com/hooks/a/j3kbdake?path=' + changedPost.responsePath + changedPost, { json: true }, (err, res, body) => {
 //      if (err) { return console.log(err); }
 //      console.log(body.url);
 //      console.log(body.explanation);
 //    });
 
-    
 });
+
+function generateAuthLink(respPath, resultKey){
+  return respPath + resultKey;
+}
+
+function createSpreadSheet(sheetData, respPath, tokens){
+  
+}
+
+function sendChatMessage(message, respPath){
+  
+}

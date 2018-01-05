a1877029b6dc2bdcd7b14a325eca2077473abd45
Alina <alechka9906@gmail.com>
2018-01-05 12:45:00 +0000

    Send AuthLink to user when FireBase changed

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+var fs = require('fs');
+var readline = require('readline');
+var google = require('googleapis');
+var googleAuth = require('google-auth-library');
+
+// If modifying these scopes, delete your previously saved credentials
+// at ~/.credentials/sheets.googleapis.com-nodejs-quickstart.json
+var SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
+var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
+    process.env.USERPROFILE) + '/.credentials/';
+var TOKEN_PATH = TOKEN_DIR + 'sheets.googleapis.com-nodejs-quickstart.json';
+
+
 var admin = require("firebase-admin");
 const request = require('request');
 
@@ -16,14 +29,25 @@ var ref = db.ref("sheets");
 ref.orderByChild("status").equalTo("new").on("child_added", function(snapshot, prevChildKey) {
   var changedPost = snapshot.val();
   console.log("The updated post title is " + changedPost.responsePath);
-    console.log(changedPost.sqlResult);
+  console.log(changedPost.sqlResult);
+  
+  // Load client secrets from a local file.
+  fs.readFile('./client_secret.json', function processClientSecrets(err, content) {
+    if (err) {
+      console.log('Error loading client secret file: ' + err);
+      return;
+    }
+    // Authorize a client with the loaded credentials, then call the
+    // Google Sheets API.
+    sendAuthLink(JSON.parse(content), changedPost.responsePath);
+  });
     
     //TODO: Check presence of auth tokens
     //TODO: If token present, create spreadsheet and send link,
     //if not generate link, send to user, generate tokens,
     // save tokens, create spreadsheet, send link
     
-//    request('https://flowxo.com/hooks/a/j3kbdake?path=' + changedPost.responsePath + changedPost, { json: true }, (err, res, body) => {
+//    request('https://flowxo.com/hooks/a/qweqx773?path=' + changedPost.responsePath + changedPost, { json: true }, (err, res, body) => {
 //      if (err) { return console.log(err); }
 //      console.log(body.url);
 //      console.log(body.explanation);
@@ -31,8 +55,27 @@ ref.orderByChild("status").equalTo("new").on("child_added", function(snapshot, p
 
 });
 
-function generateAuthLink(respPath, resultKey){
-  return respPath + resultKey;
+function sendAuthLink(credentials, respPath){
+  var clientSecret = credentials.installed.client_secret;
+  var clientId = credentials.installed.client_id;
+  var redirectUrl = credentials.installed.redirect_uris[0];
+  var auth = new googleAuth();
+  var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
+  
+  var authUrl = oauth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES
+  });
+  console.log("sendAuthLink confirm")
+  var link = generateAuthLink(authUrl, respPath);
+  sendChatMessage(link, respPath);
+  
+}
+
+function generateAuthLink(authUrl, respPath){
+  console.log("generateAuthLink confirm")
+  return authUrl + "&sourcetype=user"
+  //return authUrl + "&path=" + respPath + "&sourcetype=user";
 }
 
 function createSpreadSheet(sheetData, respPath, tokens){
@@ -40,5 +83,10 @@ function createSpreadSheet(sheetData, respPath, tokens){
 }
 
 function sendChatMessage(message, respPath){
-  
+  request('https://flowxo.com/hooks/a/qweqx773?path=' + respPath + "&message=" + message + "&sourcetype=automessage", { json: true }, (err, res, body) => {
+    if (err) { return console.log(err); }
+    console.log(body.url);
+    console.log(body.explanation);
+  });
+  console.log("sendMessage confirm")
 }
